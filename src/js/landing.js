9f57b3648e41c724579d6a238992a88797833680
Emma O'Neill <emmaruthoneill@gmail.com>
2018-01-31 17:23:05 -0800

    added in how to clicky thing

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,3 +1,11 @@
 require("./lib/social"); //Do not delete
 
-
+document.getElementById("how-to-button").addEventListener("click",function(){
+  if ($(this).find($(".fa")).hasClass("fa-angle-double-down")){
+    document.getElementById("how-to-text").classList.add("visible");
+    $(this).find($(".fa")).removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
+  } else {
+    document.getElementById("how-to-text").classList.remove("visible");
+    $(this).find($(".fa")).removeClass('fa-angle-double-up').addClass('fa-angle-double-down');
+  }
+});
