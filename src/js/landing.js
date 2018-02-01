9d64d2da31286136c385da5b7cdc8e8490de24e5
Emma O'Neill <emmaruthoneill@gmail.com>
2018-02-01 15:23:14 -0800

    cleaning things up

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,11 +1,11 @@
 require("./lib/social"); //Do not delete
 
 document.getElementById("how-to-button").addEventListener("click",function(){
-  if ($(this).find($(".fa")).hasClass("fa-angle-double-down")){
+  if ($(this).find($(".fa")).hasClass("fa-caret-down")){
     document.getElementById("how-to-text").classList.add("visible");
-    $(this).find($(".fa")).removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
+    $(this).find($(".fa")).removeClass('fa-caret-down').addClass('fa-caret-up');
   } else {
     document.getElementById("how-to-text").classList.remove("visible");
-    $(this).find($(".fa")).removeClass('fa-angle-double-up').addClass('fa-angle-double-down');
+    $(this).find($(".fa")).removeClass('fa-caret-up').addClass('fa-caret-down');
   }
 });
