ed4ced64b9dd3a5adaf2bc3fcb5d8b7fd27e0e56
ewagstaff <evanjwagstaff@gmail.com>
2019-12-31 16:00:08 -0800

    Bringing design into the next decade

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,21 +1,2 @@
 require("./lib/social"); //Do not delete
 
-document.getElementById("how-to-button").addEventListener("click",function(){
-  if ($(this).find($(".fa")).hasClass("fa-caret-down")){
-    document.getElementById("how-to-text").classList.add("visible");
-    $(this).find($(".fa")).removeClass('fa-caret-down').addClass('fa-caret-up');
-  } else {
-    document.getElementById("how-to-text").classList.remove("visible");
-    $(this).find($(".fa")).removeClass('fa-caret-up').addClass('fa-caret-down');
-  }
-});
-
-document.getElementById("credits-button").addEventListener("click",function(){
-  if ($(this).find($(".fa")).hasClass("fa-caret-down")){
-    document.getElementById("credits-text").classList.add("visible");
-    $(this).find($(".fa")).removeClass('fa-caret-down').addClass('fa-caret-up');
-  } else {
-    document.getElementById("credits-text").classList.remove("visible");
-    $(this).find($(".fa")).removeClass('fa-caret-up').addClass('fa-caret-down');
-  }
-});
